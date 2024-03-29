9a5568b40ce599f2391cc60d1c15160b50f231d2
azadeeeh <azadeeeh@yahoo.com>
2024-03-28 17:05:10 -0700

    server setup

---
+++ server/server.js
@@ -0,0 +1,35 @@
+/**
+ * Import environmental variables
+ */
+require('dotenv').config()
+
+/**
+ * Necessary imports for starting a server
+ */
+const db = require('./config/mongodb')
+const express = require('express')
+const cors = require('cors')
+const app = express()                       // Creates an express server in app
+
+/**
+ * Import routers and middleware
+ */
+const morgan = require('morgan')
+
+/**
+ * Adding middleware and routes
+ */
+app.use(morgan('dev'))
+
+/**
+ * Connect to database, start server & listen to server
+ */
+const server = async () => {
+    await db.makeConnection()
+    return app.listen(process.env.PORT, () => {
+        console.log(`Server running on PORT: ${process.env.PORT}`)
+    })
+}
+
+
+module.exports = server() 
